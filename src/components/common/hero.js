b67732a00be942c8e609ddd136fc771e1897269f
AmouletteB <amelie.barrau@gmail.com>
2023-05-19 16:30:39 +0100

    Typo Hero
    Fixed Typo in Hero

--- src/components/common/hero.js
+++ src/components/common/hero.js
@@ -7,7 +7,7 @@ const Hero=()=>{
         <div class="top-wrapper">
             <div class="middle-wrapper">
                 <h1 class="middle-wrapper-header">Get your money high</h1>
-                <p class="middle-wrapper-par">Spend, boorow and save your money in one place. 
+                <p class="middle-wrapper-par">Spend, borrow and save your money in one place. 
                     Multiply your money or risk it all. The power of the finance 
                     godes are in your hands.</p>
                     <button class="join-us">Open a Finzo account</button>
